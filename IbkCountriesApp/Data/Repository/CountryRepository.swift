7ef86a3ce0fd74009bd4bb8182de9372d42d8353
Marco Antonio Espinal <>
2024-03-25 18:12:45 -0500

    Add network connection layer.

---
+++ IbkCountriesApp/Data/Repository/CountryRepository.swift
@@ -0,0 +1,8 @@
+//
+//  CountryRepository.swift
+//  IbkCountriesApp
+//
+//  Created by Marco Antonio  on 23/03/2024.
+//
+
+import Foundation
---
+++ IbkCountriesApp/Data/Repository/CountryRepositoryProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  CountryRepositoryProtocol.swift
+//  IbkCountriesApp
+//
+//  Created by Marco Antonio  on 23/03/2024.
+//
+
+import Foundation
